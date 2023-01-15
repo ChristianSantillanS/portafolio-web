d2be1b8cacb083bae40c2195388d8e2ba92e502b
Chris_Santillan <76024382+ChristianSantillanS@users.noreply.github.com>
2023-01-14 19:49:54 -0600

    Add the header, with css and html, add one part of the Home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Social() {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/christiansilva13/" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram">
+            </i>
+        </a>
+        <a href="https://www.linkedin.com/in/christian-santillan-silva/" className="home__social-icon" target="_blank">
+            <i class="uil uil-linkedin"></i>            
+        </a>
+        <a href="https://github.com/ChristianSantillanS" className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt">
+            </i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
