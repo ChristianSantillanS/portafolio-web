04aa2815e4d3c62554031466355a5e4a9c5382d7
Christian <santillan.silva.christian@gmail.com>
2023-03-07 10:21:24 -0600

    Minimum changes

--- src/components/home/ScrollDown.jsx
+++ src/components/home/ScrollDown.jsx
@@ -4,7 +4,9 @@ function ScrollDown() {
   return (
     <div className="home__scroll">
         <a href="#about" className="home__scroll-button button--flex">
-        <i class="uil uil-mouse-alt"></i>
+        <i className="uil uil-mouse-alt home__scroll-arrow"></i>
+        <span className="home__scroll-name">Scroll Down</span>
+        <i className="uil uil-arrow-down home__scroll-arrow"></i>
         </a>
     </div>
   )
